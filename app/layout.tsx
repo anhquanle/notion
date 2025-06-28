70445cfb37c8d5c4b80dfa0725a0bd28d8853e13
anhquanle <75654249+anhquanle@users.noreply.github.com>
2025-06-28 22:41:56 +0700

    Document v2.1

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,12 @@
+import { Geist, Geist_Mono } from "next/font/google";
 import { Toaster } from "sonner";
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+
 import { ThemeProvider } from "@/components/providers/theme-provider";
+import { ModalProvider } from "@/components/providers/modal-provider";
 import { ConvexClientProvider } from "@/components/providers/convex-provider";
+
+import "./globals.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -53,6 +56,7 @@ export default function RootLayout({
             storageKey="notion-theme-2"
           >
             <Toaster position="bottom-center" />
+            <ModalProvider />
             {children}
           </ThemeProvider>
         </ConvexClientProvider>
